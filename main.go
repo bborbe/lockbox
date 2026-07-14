34d97789006cfeb61ae2953d9367ae9274c8110a
Benjamin Borbe <benjamin.borbe@gmail.com>
2026-07-14 12:38:42 +0200

    make SENTRY_DSN optional (empty disables Sentry)

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 }
 
 type application struct {
-	SentryDSN         string            `required:"true"  arg:"sentry-dsn"        env:"SENTRY_DSN"             usage:"SentryDSN"                                                                  display:"length"`
+	SentryDSN         string            `required:"false" arg:"sentry-dsn"        env:"SENTRY_DSN"             usage:"Sentry DSN (optional; empty disables Sentry)"                               display:"length"`
 	SentryProxy       string            `required:"false" arg:"sentry-proxy"      env:"SENTRY_PROXY"           usage:"Sentry Proxy"`
 	Listen            string            `required:"true"  arg:"listen"            env:"LISTEN"                 usage:"address to listen to"`
 	DataDir           string            `required:"true"  arg:"datadir"           env:"DATADIR"                usage:"data directory"`
